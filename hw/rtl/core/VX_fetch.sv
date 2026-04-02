4effa5522ac7f4e99c3ec043cbcc30aa7ea19ea3
Jens Yuechao Liu <jensliu29@gmail.com>
2026-04-02 12:47:27 +0200

    use default to_fullPC function to handle padding

--- hw/rtl/core/VX_fetch.sv
+++ hw/rtl/core/VX_fetch.sv
@@ -107,12 +107,7 @@ module VX_fetch
 
   // TODO: address translation here
   assign addr_trans_if.valid = schedule_if.valid;  // Start translating as soon as we see the VA
-  if (PC_BITS == `XLEN) begin: g_pc_fits_vaddr
-    assign addr_trans_if.va = to_fullPC(schedule_if.data.PC);  // 4-byte aligned addresses; lower 2 bits are always zero
-  end else begin: g_pc_does_not_fit_vaddr
-    // PC_BITS = `XLEN-2
-    assign addr_trans_if.va = {to_fullPC(schedule_if.data.PC), 2'b00};  // 4-byte aligned addresses; lower 2 bits are always zero
-  end
+  assign addr_trans_if.va = to_fullPC(schedule_if.data.PC);
   // Icache Request
   assign icache_req_valid = addr_trans_if.ready;  // addr_trans_if.ready implies schedule_if.valid
   assign icache_req_addr = addr_trans_if.pa[2-(`XLEN-PC_BITS)+:ICACHE_ADDR_WIDTH];  // 4-byte aligned addresses;
